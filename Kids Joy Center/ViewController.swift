03ef567a413b0678052c5c674c605a2bda440a0e
jayguan <dongchenguan@gmail.com>
2017-03-20 12:05:54 -0400

    Initial Commit

---
+++ Kids Joy Center/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kids Joy Center
+//
+//  Created by Jay Guan on 3/20/17.
+//  Copyright © 2017 Jay Guan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
